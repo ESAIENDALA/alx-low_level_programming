1c288433213773595b18f6adf0dd9dc06911e646
ESAIENDALA <esaiendala@gmail.com>
2022-12-12 08:40:42 -0800

    print the various size of tyoe

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: Always 0 (Success)
 */
-main(void)
 int main(void)
 {
-	printf("Size of a char: %ibyte(s)\n",sizeof(char));
-	printf("Size of an int: %ibyte(s)\n",sizeof(int));
-	printf("Size of a long: %ibyte(s)\n",sizeof(long int));
-	printf("Size of a long long int: %ibyte(s)\n",sizeof(long long int));
-       printf("Size of a float: %ibyte(s)\n",sizeof(float));
-printf("Size of a long long int: %i byte(s)\n",sizeof(long long int));
-printf("Size of a float: %i byte(s)\n",sizeof(float));return (0);
+        printf("Size of a char: %zu byte(s)\n", sizeof(char));
+        printf("Size of an int: %zu byte(s)\n", sizeof(int));
+        printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %zu byte(s)\n", sizeof(float));
+        return (0);
 }
